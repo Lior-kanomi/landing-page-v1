2bf40e7fd67d4cf9350ba98bbd21107ec76eb217
Lior Solomon <lior.kanomi@gmail.com>
2023-07-16 10:23:14 +0300

    add a null check

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var img = new Image();
+const img = new Image();
 img.src = "https://doodleipsum.com/300x300/outline"; // Replace with your image URL
 img.onload = function () {
   document.getElementById("doodleImage").src = img.src ?? "./doodleipsum-6.png"; // Set the source of the image element in your HTML
@@ -9,7 +9,8 @@ const searchForm = document.querySelector(".search-form");
 
 searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  var query = document.querySelector("#search-input").value;
-  var url = "https://www.google.com/search?q=" + encodeURIComponent(query);
+  const query = document.querySelector("#search-input").value;
+  if (query === "" || query === null) return;
+  const url = "https://www.google.com/search?q=" + encodeURIComponent(query);
   window.location.href = url;
 });
